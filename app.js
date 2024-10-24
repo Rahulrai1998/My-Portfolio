a5d3ee4d90d4c7212c5dd83cd492ae4298e9f76e
Rahul Kumar <rahulrai2822march@gmail.com>
2024-10-25 05:22:05 +0530

    resume added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const hamburgerButton = document.getElementById('hamburger');
-const navList = document.getElementById('nav-list');
+const hamburgerButton = document.getElementById("hamburger");
+const navList = document.getElementById("nav-list");
 
-function toggleButton(){
-    navList.classList.toggle('show')
+function toggleButton() {
+  navList.classList.toggle("show");
 }
-hamburgerButton.addEventListener('click',toggleButton);
+hamburgerButton.addEventListener("click", toggleButton);
 
+// let resume = document.querySelector("#resume");
+// let image = document.createElement("img");
+// image.src = "./icons/resume.png";
+// function toggleResumeImg() {
+//   resume.innerHTML = image;
+// }
